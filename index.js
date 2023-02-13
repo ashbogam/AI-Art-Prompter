212524a8a49c520dd0fe92888ef52ca643de18cc
ashbogam <118300470+ashbogam@users.noreply.github.com>
2023-02-13 16:26:52 -0500

    added the title, updated the positions of textbox and promt categories accordingly, changed background color.

--- index.js
+++ index.js
@@ -10,15 +10,50 @@ let styleTypes = ['painting', '3D', 'artistic','abstract','neon','sketch', 'oil 
 let final_selections=[];
 let buttonObjects=[];
 
-let app = new PIXI.Application({backgroundColor:0xFFFFFF, resizeTo: window});
+let app = new PIXI.Application({backgroundColor:0x5F0763, resizeTo: window});
 document.body.appendChild(app.view);
+
+// PIXI.Loader.shared.add("background", "/assets/background.jpeg").load((loader, resources) => {
+//     //Create a new sprite with the background image
+//     const background = new PIXI.Sprite(resources.background.texture);
+  
+//     //Set the sprite's anchor point to the center
+//     background.anchor.set(0.5);
+  
+//     //Position the sprite in the center of the stage
+//     background.x = app.screen.width / 2;
+//     background.y = app.screen.height / 2;
+  
+//     //Add the sprite to the stage
+//     app.stage.addChild(background);
+//   });
+
 // const textbox = new TextBox(200, 100, 'Hello World!');
 // app.stage.addChild(textbox);
+
+const text = new PIXI.Text("AI Art Prompter", {
+    fontFamily: "Roboto",
+    fontSize: 36,
+    fill: 0xffffff,
+    align: "center"
+  });
+  
+  // Set the position of the Text object
+  text.x = app.screen.width / 2;
+  text.y = 100;
+  
+  // Center the Text object's anchor point
+  text.anchor.set(0.5);
+  
+  // Add the Text object to the PixiJS Application's stage
+  app.stage.addChild(text);
+
+
 let textInputBoxX = 500;
-let textInputBoxY = 100;
+let textInputBoxY = 200;
 let textInputBoxWidth = 1000;
 let textInputBoxHeight = 50;
-let textInputBox = new TextInputBox(textInputBoxX, textInputBoxY, textInputBoxWidth, textInputBoxHeight, 'Enter text here', new TextStyle({ fill: 'black' }));
+let textInputBox = new TextInputBox(textInputBoxX, textInputBoxY, textInputBoxWidth, textInputBoxHeight, 'Enter text here', new TextStyle({ fill: 'white' }));
 app.stage.addChild(textInputBox);
 
 const buttonWidth = 150;
